8607f35d37b60f88545731ce734dd859d7b7128d
kumkum99 <kumkumkumari8899@gmail.com>
2024-04-09 15:07:38 +0530

    frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'shanti_hospital';
+export class AppComponent implements OnInit {
+  public animatedTextArray: string[] = [];
+  
+  constructor() {}
+  
+  ngOnInit(): void {
+    this.typeWriter(" to Our Hospital Shanti Clinic!😊");
+  }
+  
+  typeWriter(text: string) {
+    const interval = 200; // Adjust typing speed here (200ms)
+    let i = 0;
+    const typingInterval = setInterval(() => {
+      if (i < text.length) {
+        this.animatedTextArray.push(text.charAt(i));
+        i++;
+      } else {
+        clearInterval(typingInterval); // Stop the typing animation when typing completes
+      }
+    }, interval);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
